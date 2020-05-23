9fe560410d138901aa06f536d226124531d75af3
yassine <yassine.lf.99@gmail.com>
2020-05-23 01:41:51 +0000

    course one

---
+++ course1.py
@@ -0,0 +1,25 @@
+
+for i in range(10):
+    print("hello world " + str(i))
+
+var = 10
+while var > 0:
+    print("hello world " + str(var))
+    var = var - 1 # var-=1 
+
+for i in range(len("houssam")):
+    print(i)
+
+listt = [12,"mohamed"]
+for i in listt:
+    print(i)
+
+strr = "hello"
+listt1 = ["test","test",14]
+listt2 = [21,14,32]
+listt1.append(13)
+tuplee = (12,14,14)
+print(listt1)
+print(set(listt1))
+
+
